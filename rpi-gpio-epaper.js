5c7dce98d9df77ea85359eea688559a83aef7c06
David Bushell <david@dbushell.com>
2017-09-14 10:30:25 +0100

    module exports

--- rpi-gpio-epaper.js
+++ rpi-gpio-epaper.js
@@ -1,3 +1,12 @@
-const {EPD} = require('./lib/epd');
+const {EPD, logT, logE, log1, log2, log3, log4, sleep} = require('./lib/epd');
 
-module.exports = EPD;
+module.exports = {
+  EPD: EPD,
+  logT: logT,
+  logE: logE,
+  log1: log1,
+  log2: log2,
+  log3: log3,
+  log4: log4,
+  sleep: sleep
+};
